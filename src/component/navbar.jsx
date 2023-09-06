d6f48db1f7b6e316f00d65cf3b696bb2699af892
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-09-06 22:00:10 +0700

    selesai buat component header home

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ()=>{
     return (
         <nav className="main_navbar">
             <div className="container_nav max-w-[1000px] flex items-center h-[60px] mx-auto">
-                <h2 className="text-white font-bold text-xl uppercase">to do list app</h2>
+                <h2 className="text-white font-bold text-[1.5em] uppercase">to do list app</h2>
             </div>
         </nav>
     )
--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -3,11 +3,24 @@ import React from "react";
 
 import PagesLayout from "../layout/pagesLayout";
 
+// import icons
+import { BiPlus } from "react-icons/bi";
 
 const HomePage=  () =>{
     return (
         <PagesLayout title='Homepage'>
-            
+            <div className="max-w-[1020px] px-2 mx-auto mt-[2em]">
+                {/* header home page */}
+                <header className="header_homepage flex flex-wrap justify-between items-center">
+                    <h2 className="font-bold text-[1.8em]">
+                        Activity
+                    </h2>
+                    <button className="btn_add_activity">
+                            <BiPlus className="text-[1.3em] font-bold" />
+                            <p className="">Tambah</p>
+                    </button>
+                </header>
+            </div>
         </PagesLayout>
     )
 }
