4d707eafed13de3a3d8bf392a76d6994359293e3
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-09-07 20:37:33 +0700

    mulai fitur get data activity

--- src/component/emptyComponent.jsx
+++ src/component/emptyComponent.jsx
@@ -4,12 +4,12 @@ import React from "react";
 import iconEmptyActivity from '../assets/images/activity-empty-state.png'
 
 // import function post data activity
-import addDataActivity from "../store/activity";
+import addDataActivity from "../utils/activity";
 
 
 // component empty activity
 const EmptyActivity = ()=>{
-
+ 
     return (
         <section onClick={addDataActivity.bind(this,'gagal menambahkan data activity')}  className="empty_activity cursor-pointer relative w-full flex justify-center pt-5">
                 <img src={iconEmptyActivity} className="mx-auto w-[900px]  h-[400px] object-contain  inline-block" alt=""  />
