54ce8c2712a9991abc4ebe8fa7e1f5ef03d136d7
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-09-11 22:38:31 +0700

    selesai buat component header di detailPageActivity

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -5,8 +5,8 @@ import React from "react";
 const Navbar = ()=>{
     return (
         <nav className="main_navbar">
-            <div className="container_nav max-w-[1000px] flex items-center h-[60px] mx-auto">
-                <h2 className="text-white font-bold text-[1.5em] uppercase">to do list app</h2>
+            <div data-cy="header-background" className="container_nav max-w-[1000px] flex items-center h-[60px] mx-auto">
+                <h2 data-cy="header-title" className="text-white font-bold text-[1.5em] uppercase">to do list app</h2>
             </div>
         </nav>
     )
--- src/pages/detailPage.jsx
+++ src/pages/detailPage.jsx
@@ -14,21 +14,24 @@ import { Link } from "react-router-dom";
 const DetailPage = ()=>{
     return (
         <PagesLayout title="detailActivity" >
-            <section className="max-w-[1020px] px-2 mx-auto mt-[2em] border-2 border-red-600">
+            <section className="max-w-[1020px] px-2 mx-auto mt-[2em] ">
                 {/* header Detail Activty */}
                 <header className="header_detail_activity">
                     {/* icon nav */}
-                    <div className="flex-1 flex w-full border-2 border-red-600">
+                    <div className="flex-1 flex w-full border-b-[1px] border-b-slate-200 py-2 md:border-0 md:py-0">
                         <Link to={'/'} className="self-center">
                             <IoIosArrowForward className=" inline-block text-[2em]  font-semibold  rotate-[-180deg] md:text-[2.8em]"/>
                         </Link>
 
                         {/* title activty */}
-                        <div className="flex-1 w-full border-2 border-cyan-600">
-                            <div className="flex  items-center border-2 border-red-600 h-full ">
-                                <h2 className="title_activty pr-3 text-[1.4em]  font-semibold cursor-pointer md:text-[1.8em]">
+                        <div className="flex-1 w-full ">
+                            <div className="flex  items-center  h-full ">
+                                {/* title detail activity */}
+                                <h2 className="title_activty pr-3 flex-1  text-[1.4em]  font-semibold cursor-pointer md:text-[1.4em] md:flex-initial">
                                     New Activity
                                 </h2>
+                                {/* input change detail activity */}
+                                {/* <input type="text" className=" flex-1 w-full py-1 bg-transparent text-semibold outline-none border-b-[1px] border-b-slate-200 text-[1.3em] md:text-[1.4em]" /> */}
                                 <button className="py-1">
                                     <TbPencil className="text-[1.5em] text-slate-400 font-medium"/>
                                 </button>
@@ -38,11 +41,12 @@ const DetailPage = ()=>{
                     
                     {/* action nav add todo */}
                     <div className="nav_item_add_todo">
-
+                        {/* button devide sort todo */}
                         <button className="btn_sort_todo h-[40px] w-[40px] rounded-full bg-slate-100 border-[1px] border-slate-300 grid place-items-center md:h-[45px] md:w-[45px]" >
                             <img src={tableArrowsSort} className="object-center " alt="" />
                         </button>
 
+                        {/* button add todo */}
                         <button className="relative flex items-center gap-[2px] h-[40px]  justify-center rounded-full w-auto px-3  transition-all duration-300 hover:opacity-[0.6] text-white bg-blue-navbar py-2.5 md:h-[45px]">
                             <BiPlus className="text-[0.9em] md:text-[1em] font-bold" />
                             <p className="text-[0.9em] md:text-[1em] font-semibold">Tambah</p>
