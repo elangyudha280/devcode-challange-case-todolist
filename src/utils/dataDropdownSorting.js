f69afdd8daecb12626ea1d30a695c8378398e390
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-09-30 20:24:41 +0700

    Selesai buat data sorting

---
+++ src/utils/dataDropdownSorting.js
@@ -0,0 +1,42 @@
+import IconTerbaru from '../assets/images/icon_sort_terbaru.svg'
+import IconTerlama from '../assets/images/icon_sort_terlama.svg'
+import IconSrotingAZ from '../assets/images/icon_abjad_first.svg'
+import IconSortingZA from '../assets/images/icon_abjad_last.svg'
+import IconSortingBelumSelesai from '../assets/images/belum_Selesai.svg'
+
+
+
+const dropdownSorting = [
+    {
+        id:1,
+        title:"terbaru",
+        type:'terbaru',
+        icon:IconTerbaru
+    },
+    {
+        id:2,
+        title:"terlama",
+        type:'terlama',
+        icon:IconTerlama
+    },
+    {
+        id:3,
+        title:"A-Z",
+        type:'A-Z',
+        icon:IconSrotingAZ
+    },
+    {
+        id:4,
+        title:"Z-A",
+        type:'Z-A',
+        icon:IconSrotingZA
+    },
+    {
+        id:5,
+        title:"belum selesai",
+        type:'belum selesai',
+        icon:IconSortingBelumSelesai
+    },
+]
+
+export default dropdownSorting;
