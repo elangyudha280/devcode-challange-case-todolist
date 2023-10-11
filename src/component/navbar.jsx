75b56f172928605a72aa3368965fa8fa22e714f3
elangyudha280 <elangyudharakasiwi@gmail.com>
2023-10-11 21:20:33 +0700

    fix

--- src/component/navbar.jsx
+++ src/component/navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = ()=>{
     return (
         <nav className="main_navbar">
             <div data-cy="header-background" className="container_nav max-w-[1000px] flex items-center h-[60px] mx-auto">
-                <h1 data-cy="header-title" className="text-white font-bold text-[1.5em] uppercase">to do list app</h1>
+                <h1 data-cy="header-title" className="text-white font-bold text-[1.5em] uppercase">TO DO LIST APP</h1>
             </div>
         </nav>
     )
